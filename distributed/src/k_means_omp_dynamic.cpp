0b1253eae4ab7ee9a10cbe055360b2a30e8bfe46
eliazonta <elia.zontaa@gmail.com>
2024-02-07 13:21:56 +0100

    omp static and dynamic algorithm

--- distributed/src/k_means_omp_dynamic.cpp
+++ distributed/src/k_means_omp_dynamic.cpp
@@ -11,14 +11,76 @@
 double KMeans(std::vector<Observation> &points, std::vector<Observation> &centroids,
               unsigned int epochs, double tolerance)
 {
-    #pragma omp parallel for
-    for (size_t i = 0; i < 100; ++i) {
-        std::cout << i <<std::endl;
-    }
+    auto start_time = std::chrono::high_resolution_clock::now();
+  
+    auto fs = points[0].getFeatureSize();
+
+    std::vector<std::vector<double>> cumulative(centroids.size(), std::vector<double>(fs, 0.0)); // cumulative sum of features for each cluster: [clusterID][featureID]
     
-    std::cout << "I'm the dynamic omp code!" << std::endl;
-    return 0.0;
+    std::vector<int> counter(centroids.size(), 0); // number of points in each cluster 
+    
+
+    for (size_t e = 0; e < epochs; ++e)
+    {
+        auto delta = 0.0;
+        auto moved = false;
+
+        #pragma omp parallel for schedule(dynamic, points) reduction(+:delta) reduction(||:moved)
+        for (auto &p : points)
+        {
+            double dist = std::numeric_limits<double>::max();
+
+            #pragma omp parallel for schedule(dynamic, centroids) reduction(min:dist)
+            for (const auto &centroid : centroids)
+            {
+                const auto cid = centroid.getClusterID();
+
+                const auto newDist = distance(p.getPoint(), centroid.getPoint());
+
+                if (newDist < dist)
+                {
+                    p.setClusterID(cid);
+                    dist = newDist;
+                    for(size_t j = 0; j < fs; ++j)
+                    {
+                        cumulative[cid][j] += p.getFeatures(j); // update the counter when adding the pt at the cluster 
+                    }
+                    counter.at(cid)++;
+                    moved = true;
+                }
+            }
+        }
+
+        if (!moved)
+            break;
+
+        #pragma omp parallel for schedule(dynamic, centroids) reduction(+:delta)
+        for(size_t i = 0; i < centroids.size(); ++i)
+        {
+            auto old_centroid = centroids[i]; 
+
+            #pragma omp parallel for schedule(dynamic, fs)
+            for(size_t j = 0; j < fs; ++j)
+            {
+                centroids[i].setFeatures(j, counter[i] == 0 ? 0.0 : cumulative[i][j] / counter[i]);
+
+                counter[i] = 0;  // reset 
+                cumulative[i][j] = 0.0; // reset 
+            }
+            delta += distance(centroids[i].getPoint(), old_centroid.getPoint());
+        }
+        if(delta < tolerance)
+            break;
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end_time - start_time;
+
+    std::chrono::milliseconds ms = std::chrono::duration_cast< std::chrono::milliseconds >( elapsed );
+
+    return elapsed.count();
 }
+
 
 #else
 
